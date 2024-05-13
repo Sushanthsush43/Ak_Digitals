32fbbc78beeba004ca0af84a20c45fa1ed86c459
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-05-13 16:42:17 +0530

    fixing ios video problem

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -174,29 +174,28 @@ function VideoContainer({storage}) {
                 as="video"
                 className='image-video'
                 key={index}
-                // onChange={(inView, entry) => {
-                //   // Trigger inView callback even before fully visible
-                //   if (entry.isIntersecting || entry.boundingClientRect.top < 100) {
-                //     inView && loaded ? (videoUrl = videoUrl) : (videoUrl = '');
-                //   }
-                // }}
-                // onMouseEnter={(e) => { handlePlay(e.target); videoUrl = videoUrl; thumbnailUrl = thumbnailUrl}}
-                // onMouseLeave={(e) => { handlePause(e.target); videoUrl = ''; thumbnailUrl = false}}
-                // onLoadedData={() => handleVideoLoad(index)}
-                // src={videoUrl}
+                onChange={(inView, entry) => {
+                  // Trigger inView callback even before fully visible
+                  if (entry.isIntersecting || entry.boundingClientRect.top < 100) {
+                    inView && loaded ? (videoUrl = videoUrl) : (videoUrl = '');
+                  }
+                }}
+                onMouseEnter={(e) => { handlePlay(e.target); videoUrl = videoUrl; thumbnailUrl = thumbnailUrl}}
+                onMouseLeave={(e) => { handlePause(e.target); videoUrl = ''; thumbnailUrl = false}}
+                onLoadedData={() => handleVideoLoad(index)}
+                src={videoUrl}
                 poster={thumbnailUrl}
-                // onError={(e) => console.error('Error playing video while hover (hover):', e.target.error)}
+                onError={(e) => console.error('Error playing video while hover (hover):', e.target.error)}
                 alt={`Video ${index}`}
                 data-index={index}
                 onClick={() => viewVideo(videoUrl, index)} // Click to open video in full-screen
-                // style={{ display: loaded ? 'inline' : 'none', cursor : 'pointer', backgroundColor:'white'}}
+                // style={{ display: loaded ? 'inline' : 'none', cursor : 'pointer'}}
                 autoPlay={false}
                 muted
                 playsInline
                 // controls
                 type="video/mp4/mov"
               >
-                <source type="video/mp4/mov" src={videoUrl} />
               </InView>
 
             //   <video autoPlay={false} muted id="video-bg" className='image-video'
