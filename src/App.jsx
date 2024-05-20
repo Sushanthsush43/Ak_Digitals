310c9b999ee335b905eb738e8b29a1d4dc6d6a6a
Meghanath Shetty <meghanshetty3122@gmail.com>
2024-05-20 21:15:30 +0530

    something

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import DashBoard from './pages/DashBoard';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/dashboard" exact component={DashBoard} />
-        <Route path="/contactpage" exact component={ContactPage} />
+        <Route path="/contactpage" exact component={AboutUs} />
       </Switch>
     </Router>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -42,10 +42,10 @@ function HomePage() {
       
       {/* <VideoContainer /> */}
       {/* <AdminLoginForm app={app}/> */}
-      {/* <TabsComponent storage={storage} Tab1={PhotoContainer} Tab2={VideoContainer}/> */}
+      <TabsComponent storage={storage} Tab1={PhotoContainer} Tab2={VideoContainer}/>
       {/* <DeletePhotos storage={storage}/> */}
       {/* <DeleteVideos storage={storage}/> */}
-      <AboutUs/>
+      {/* <AboutUs/> */}
       {/* <PhotoUpload storage={storage}/> */}
       {/* <VideoUpload storage={storage}/> */}
     </div>
