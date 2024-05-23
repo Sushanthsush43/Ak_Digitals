22b491f7dff978d49fb5e12b5fa0ae9dd2c93abc
Rakshith Rangaraj poojary <poojaryrakshith042@gmail.com>
2024-05-23 15:53:23 +0530

    admin login

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -119,6 +119,9 @@ function DashBoard({ storage, app }) {
                         <Link to='/deletevideos' className="teko-headings">Videos</Link>
                       </div>
                   </div>
+                  <div>
+                    
+                  </div>
               </div>
           </div>
   );
